f4b683f9dc760e402447804c5a8eb31806c7bd62
etctera <yilonglei790@gmail.com>
2023-10-16 21:29:57 +0800

    feat: update version to 1.1.7

--- packages/commitlint-config/index.js
+++ packages/commitlint-config/index.js
@@ -12,6 +12,10 @@ module.exports = {
     'subject-full-stop': [2, 'never', '.'],
     'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'test', 'refactor', 'chore', 'revert']],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'test', 'refactor', 'chore', 'revert'],
+    ],
   },
 };
--- packages/eslint-config/typescript/vue.js
+++ packages/eslint-config/typescript/vue.js
@@ -1,10 +1,6 @@
 module.exports = {
-  extends: [
-    './index',
-    '../rules/vue',
-  ].map(require.resolve),
+  extends: ['./index', '../rules/vue'].map(require.resolve),
   parserOptions: {
-    // https://github.com/mysticatea/vue-eslint-parser#parseroptionsparser
     parser: '@typescript-eslint/parser',
   },
 };
